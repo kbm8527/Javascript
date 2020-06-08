2d51902d3da7122cd160b0dce699b6e80b0119fd
kbm8527 <kbm8527@gmail.com>
2020-06-08 17:46:30 +0900

    2020/06/08 Ch02

---
+++ Ch02/lib/calc.js
@@ -0,0 +1,20 @@
+function plus(x,y){
+    var z = x + y;
+    return z;
+
+}
+function minus(x,y){
+    var z = x - y;
+    return z;
+
+}
+function multi(x,y){
+    var z = x * y;
+    return z;
+
+}
+function div(x,y){
+    var z = x / y;
+    return z;
+
+}
